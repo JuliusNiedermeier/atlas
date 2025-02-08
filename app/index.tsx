8da1dcaf59354ed96375979969350a871d089ca6
JuliusNiedermeier <julius.niedermeier@outlook.de>
2025-02-08 21:38:52 +0100

    Added edge-to-edge display

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,19 @@
 import { Text, View } from "react-native";
+import { SystemBars } from "react-native-edge-to-edge";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <>
+      <SystemBars style="auto" />
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text>Edit app/index.tsx to edit this screen.</Text>
+      </View>
+    </>
   );
 }
